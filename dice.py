ed660fb97cd2de8a6f6781af45f581ecaf1256ca
Milena Napierała <m.napierala24@gmail.com>
2023-02-24 22:08:27 +0100

    new

--- dice.py
+++ dice.py
@@ -4,4 +4,12 @@ dice_sides = 6
 throw_result = randint(1, dice_sides)
 
 print('The number is: ')
-print(throw_result)
+
+for i in range(6):
+    throw_result = randint(1, dice_sides)
+    print('The number is: ')
+    print(throw_result)
+    input()
+   
+
+#TODO history of throw numbers
