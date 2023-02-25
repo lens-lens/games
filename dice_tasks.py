c3fb0720fc0080cb564780476419be38598b0c6d
Milena Napierała <m.napierala24@gmail.com>
2023-02-25 11:43:08 +0100

    pseudokod

--- dice_tasks.py
+++ dice_tasks.py
@@ -1,5 +1,17 @@
 import random
 
+#TODO PSEUDOKOD - szczegółowe krok po kroku to do:
+"""
+funkcja 
+    input nic
+    output list intow od 1 do 6 w losowej kolejnosci
+
+    > a <- lista od 1 do 6
+    > b <- pomieszana list a
+    > zwróć b
+"""
+
+#TODO KOD - prawidłowy kod na podstawie pseudokodu:
 def draw_task():
     numbers = [1, 2, 3, 4, 5, 6]
     random.shuffle(numbers)
