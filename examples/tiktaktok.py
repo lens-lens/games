dea9515ba9160b96c900f1198bc83560a2ee2d2e
Milena Napierała <m.napierala24@gmail.com>
2023-04-03 10:56:10 +0200

    new game

---
+++ examples/tiktaktok.py
@@ -0,0 +1,36 @@
+'''
+input: 
+1) pusta plansza
+output: stan gry
+'''
+#TODO 1: pusta plansza
+field_00 = ''
+field_01 = ''
+field_02 = ''
+
+field_10 = ''
+field_11 = ''
+field_12 = ''
+
+field_20 = ''
+field_21 = ''
+field_22 = ''
+
+level_1 = [field_00, field_01, field_02]
+level_2 = [field_10, field_11, field_12]
+level_3 = [field_20, field_21, field_22]
+
+game_field = [level_1, level_2, level_3]
+# print(game_field)
+
+for level in game_field:
+    print(level)
+
+#TODO 2: wpis uytkownika
+x, y = map(int, input('Wybierz pole od 0 0 do 2 2: ').split())
+print(f'Your x is {x} and your y is {y}')
+game_field[0][1] = 'x'
+# print(game_field)
+
+for level in game_field:
+    print(level)
